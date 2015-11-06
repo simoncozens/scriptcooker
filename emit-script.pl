8a8722e11625808ac8ee555d5b1c7d33fea81585
Simon Cozens <simon@simon-cozens.org>
2015-11-06 12:21:50 +0900

    Bunch more scripts; get UHDR automatically; also include names; guess Noto path.

--- emit-script.pl
+++ emit-script.pl
@@ -1,14 +1,26 @@
 use Unicode::UCD qw(charinfo charscript);
 use JSON::XS;
 use File::Slurp;
+use LWP::Simple;
 my $database = decode_json(read_file("scripts.json"));
 
 for my $n (keys %$database) {
   my @l = @{charscript($n)};
   my @cps;
   for (@l) { push @cps, $_ for $_->[0]..$_->[1] };
-  $database->{$n}{count} = $#cps;
-  $database->{$n}{codepoints} = \@cps;
+  $database->{$n}{codepoints} = [ map { [$_, charinfo($_)->{name}]} @cps ];
+  my $code;
+  if ($code = $database->{$n}{udhr} and $code =~ /^\w+$/) {
+    warn "Getting http://unicode.org/udhr/d/udhr_".$code.".txt\n";
+    my $udhr = get("http://unicode.org/udhr/d/udhr_".$code.".txt");
+    if (!$udhr) { warn " Failed!\n"}
+    $udhr =~ s/.*?^---\s+//ms;
+    $udhr =~ /(.*\n.*\n.*\n.*)/;
+    if ($1) { $udhr = $1;
+      $udhr =~ s/\s+/ /msg;
+      $database->{$n}{udhr} = $udhr;
+    }
+  }
 }
 
 print "ScriptCooker.scripts = " . encode_json($database);
